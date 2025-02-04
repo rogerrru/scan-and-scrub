3f9a6e2f2ba167c3bf01118f2f2ea6815fda96be
roger <2223481@slu.edu.ph>
2025-02-04 23:26:47 +0800

    added simple js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    let navbarCollapse = document.querySelector(".navbar-collapse");
+
+    window.addEventListener("scroll", function () {
+        if (navbarCollapse.classList.contains("show")) {
+            navbarToggler.click(); // Closes navbar when scrolling
+        }
+    });
+
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: "smooth"
+            });
+        });
+    });
+});
