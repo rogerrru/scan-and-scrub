e6a46f61ef32f4d65187beffecb95d45bd9755bb
Haydee Shane Saguid <2211010@slu.edu.ph>
2025-02-23 01:15:37 +0800

    modify footer and procedures page design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import logo from "/logo-icon.png"; 
+import logo from "/logo-icon-2.png"; 
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-center py-6 border-t border-gray-200">
-      <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
-
+    <footer className="bg-[#2E6AD9] text-center py-4 border-t border-gray-200">
+      <div className="flex justify-center">
+        <div className="w-100 h-[5px] bg-white mb-4 "></div>
+      </div>
       <div className="flex justify-center mb-2">
-        <img src={logo} alt="Scan&Scrub Logo" height="40" className="h-10" />
+        <img src={logo} alt="Scan&Scrub Logo" height="40" className="h-15 w-auto" />
       </div>
 
-      <p className="text-gray-600 text-sm">©2025 Scan&Scrub. All Rights Reserved</p>
+      <p className="text-white font-light text-sm">©2025 Scan&Scrub. All Rights Reserved</p>
     </footer>
   );
 };
--- src/pages/Procedures.jsx
+++ src/pages/Procedures.jsx
@@ -1,16 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/index.css'
 
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-
 const Procedures = () => {
+  const [searchProcedure, setSearchProcedure] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("major");
+
+  // hard coded for testing
+  const procedures = {
+    major: [
+      { id: 1, procedure_name: "Heart Surgery" },
+      { id: 2, procedure_name: "Brain Surgery" },
+      { id: 3, procedure_name: "Organ Transplant" },
+    ],
+    minor: [
+      { id: 4, procedure_name: "Stitches Removal" },
+      { id: 5, procedure_name: "Wound Dressing" },
+    ],
+  };
+
   return (
     <div>
       <Header />
       
+      <div className="container mx-auto px-5 py-10">
+        <h1 className="text-5xl md:text-6xl text-[#2E6AD9] font-bold text-center mb-6 mt-10">
+          Operating Room Procedures
+        </h1>
+
+        {/* Search Bar */}
+        <div className="flex justify-center m-15 px-4">
+          <div className="relative w-full max-w-md ">
+            <input
+              type="text"
+              placeholder="Search for Procedures..."
+              value={searchProcedure}
+              onChange={(e) => setSearchProcedure(e.target.value)}
+              className="border-2 border-[#2E6AD9] px-10 py-4 pr-16 rounded-full w-full font-semibold placeholder-[#716C68]"
+            />
+            <button className="bg-[#2E6AD9] absolute right-2 top-1/2 transform -translate-y-1/2 text-white px-10 py-3 rounded-full hidden sm:block cursor-pointer">
+              Search
+            </button>
+          </div>
+        </div>
+
+        {/* Category Buttons */}
+        <div className="flex justify-center gap-6 mb-6s">
+          <button
+            onClick={() => setSelectedCategory("major")}
+            className={`px-6 py-2 rounded-md font-medium text-2xl cursor-pointer ${
+              selectedCategory === "major"
+                ? "bg-[#2E6AD9] text-white"
+                : "bg-[white] text-[#2E6AD9] border-3 border-[#2E6AD9]"
+            }`}
+          >
+            Major Procedures
+          </button>
+          <button
+            onClick={() => setSelectedCategory("minor")}
+            className={`px-6 py-2 rounded-md font-medium text-2xl cursor-pointer ${
+              selectedCategory === "minor"
+                ? "bg-[#2E6AD9] text-white"
+                : "bg-[white] text-[#2E6AD9] border-3 border-[#2E6AD9]"
+            }`}
+          >
+            Minor Procedures
+          </button>
+        </div>
+
+        {/* Procedure Cards */}
+        <div className="overflow-x-auto whitespace-nowrap flex gap-4 p-4">
+          {procedures[selectedCategory].map((procedure) => (
+            <div
+              key={procedure.id}
+              className="w-60 h-40 bg-white shadow-md rounded-lg p-4 flex items-center justify-center text-lg font-semibold border"
+            >
+              {procedure.name}
+            </div>
+          ))}
+        </div>
+      </div>
 
       <Footer />
     </div>
